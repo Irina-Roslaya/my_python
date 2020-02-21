3c4007a5f1835af0b8558195ee459a9a5109e6c7
Irina Roslaya <irina.roslaya@gmail.com>
2020-02-22 02:29:10 +0300

    init commit

---
+++ my_max.py
@@ -0,0 +1,8 @@
+first_element = input("Введите значение первого элемента : ")
+second_element = input("Введите значение второго элемента : ")
+def my_max(x,y):
+    if x>y:
+        return x
+    else:
+        return y
+    
