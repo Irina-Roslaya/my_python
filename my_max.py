2d1dc7ae0cf67773935f03ba95a89675b53dde83
Irina Roslaya <irina.roslaya@gmail.com>
2020-02-22 03:03:45 +0300

    init commit

--- my_max.py
+++
@@ -1,9 +0,0 @@
-firstel = float(input("Введите значение первого элемента : "))
-secondel = float(input("Введите значение второго элемента : "))
-def my_max(firstel,secondel):
-    if firstel > secondel:
-        return firstel
-    else:
-        return secondel
-    
-print(my_max(firstel,secondel))
